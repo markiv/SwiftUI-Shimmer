e73fcb0128be3b50c10734b1f1b862a4d673323a
Aswath Narayanan <aswathrce@gmail.com>
2021-11-04 22:03:26 +0530

    Adding a ton of functionality to control animation

--- Sources/Shimmer/Shimmer.swift
+++ Sources/Shimmer/Shimmer.swift
@@ -14,25 +14,41 @@ import SwiftUI
 public struct Shimmer: ViewModifier {
     
     @State private var phase: CGFloat = 0
-    var duration: Double
-    var bounce: Bool
-    
+    var movement: AnimationMovement
+    var delay: Double
+    @State var contentSize: CGSize = .zero
+    var bounces: Bool
     let invertedMask: Bool
+    var repeats: Bool
     
-    public init(duration: Double = 1.5, bounce: Bool = false, invertedMask: Bool = false) {
+    public init(movement: AnimationMovement = .constantDuration(1.5), delay: Double = 0, bounces: Bool = false, invertedMask: Bool = false, repeats: Bool = true) {
         
-        self.duration = duration
-        self.bounce = bounce
+        self.movement = movement
+        self.delay = delay
+        self.bounces = bounces
         self.invertedMask = invertedMask
+        self.repeats = repeats
     }
 
     public func body(content: Content) -> some View {
-        content
-            .modifier(AnimatedMask(phase: phase, invertedMask: invertedMask).animation(
-                Animation.linear(duration: duration)
-                    .repeatForever(autoreverses: bounce)
-            ))
-            .onAppear { phase = 0.8 }
+        Group {
+            
+            content
+                .modifier(AnimatedMask(phase: phase, invertedMask: invertedMask)
+                            .animation(Animation.animation(movement: movement, contentSize: contentSize, delay: delay, repeats: repeats, bounces: bounces)))
+                .onAppear { phase = 0.8 }
+            
+                .background(GeometryReader { geoProxy in
+                    
+                    Rectangle()
+                        .fill(.clear)
+                        .preference(key: ContentSizeSetterPreferenceKey.self, value: [ContentSizeSetterPreferenceData(size: geoProxy.size)])
+                }, alignment: .center)
+        }
+        .onPreferenceChange(ContentSizeSetterPreferenceKey.self, perform: { prefs in
+            
+            contentSize = prefs.first?.size ?? .zero
+        })
     }
 
     /// An animatable modifier to interpolate between `phase` values.
@@ -53,11 +69,11 @@ public struct Shimmer: ViewModifier {
         func body(content: Content) -> some View {
             
             if !invertedMask {
-                
-                content.mask(GradientMask.shimmerGradientMask(phase).scaleEffect(3))
+
+                content.mask(GradientMask.shimmerGradientMask(phase).scaleEffect(2))
             } else {
                 
-                content.mask(GradientMask.invertShimmerGradientMask(phase).scaleEffect(3))
+                content.mask(GradientMask.invertShimmerGradientMask(phase).scaleEffect(2))
             }
         }
     }
@@ -80,8 +96,8 @@ public struct Shimmer: ViewModifier {
             LinearGradient(gradient:
                 Gradient(stops: [
                     .init(color: edgeColor, location: phase),
-                    .init(color: centerColor, location: phase + 0.1),
-                    .init(color: edgeColor, location: phase + 0.2)
+                    .init(color: centerColor, location: phase + 0.15),
+                    .init(color: edgeColor, location: phase + 0.3)
                 ]), startPoint: .topLeading, endPoint: .bottomTrailing)
         }
     }
@@ -95,10 +111,9 @@ public extension View {
     ///   - duration: The duration of a shimmer cycle in seconds. Default: `1.5`.
     ///   - bounce: Whether to bounce (reverse) the animation back and forth. Defaults to `false`.
     @ViewBuilder func shimmering(
-        active: Bool = true, duration: Double = 1.5, bounce: Bool = false, invertedMask: Bool = false
-    ) -> some View {
+        active: Bool = true, movement: Shimmer.AnimationMovement = .constantDuration(1.5), bounces: Bool = false, invertedMask: Bool = false, repeats: Bool = true) -> some View {
         if active {
-            modifier(Shimmer(duration: duration, bounce: bounce, invertedMask: invertedMask))
+            modifier(Shimmer(movement: movement, bounces: bounces, invertedMask: invertedMask, repeats: repeats))
         } else {
             self
         }
@@ -114,13 +129,69 @@ extension Shimmer.GradientMask {
     
     static func invertShimmerGradientMask(_ phase: CGFloat) -> Shimmer.GradientMask {
         
-        Shimmer.GradientMask(phase: phase, centerColor: .white.opacity(0.3), edgeColor: .white.opacity(1))
+        Shimmer.GradientMask(phase: phase, centerColor: .black.opacity(0.5), edgeColor: .black.opacity(1))
     }
 }
 
 extension Shimmer.AnimatedMask {
     
     
+}
+
+public extension Shimmer {
+    
+    enum AnimationMovement {
+        
+        case constantVelocity(Double) //Assuming velocity of 1 => traverse screen width in 1 second
+        case constantDuration(Double)
+    }
+}
+
+public extension Shimmer.AnimationMovement {
+
+    func duration(with contentSize: CGSize) -> Double {
+
+        switch self {
+
+        case .constantDuration(let dur): return dur
+
+        case .constantVelocity(let vel):
+            let screenWidth = UIScreen.main.bounds.size.width
+            let contentWidth = contentSize.width
+            return contentWidth / screenWidth / vel
+        }
+    }
+}
+
+extension Shimmer {
+    //Setting this preference key to precicely get and set
+    struct ContentSizeSetterPreferenceData: Equatable {
+        
+        let size: CGSize
+    }
+    
+    struct ContentSizeSetterPreferenceKey: PreferenceKey {
+        static var defaultValue: [ContentSizeSetterPreferenceData] = []
+        
+        static func reduce(value: inout [ContentSizeSetterPreferenceData], nextValue: () -> [ContentSizeSetterPreferenceData]) {
+            value.append(contentsOf: nextValue())
+        }
+    }
+}
+
+extension Animation {
+    
+    static func animation(movement: Shimmer.AnimationMovement, contentSize: CGSize, delay: Double = 0, repeats: Bool = true, bounces: Bool = false) -> Animation {
+        
+        if repeats {
+            return Animation.linear(duration: movement.duration(with: contentSize))
+                .delay(delay)
+                .repeatForever(autoreverses: bounces)
+        } else {
+            return Animation.linear(duration: movement.duration(with: contentSize))
+                .delay(delay)
+        }
+    }
 }
 
 #if DEBUG
